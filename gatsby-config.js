473204ca204c19c69389c3a8c60177a1670c9375
jsephora <joey@sephoradesign.com>
2023-04-02 20:55:03 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Joeys new website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
