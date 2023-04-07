638e9ddbaff55aede279adb4592bf2041a227242
jsephora <joey@sephoradesign.com>
2023-04-08 08:31:43 +0900

    added data and blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,17 @@ module.exports = {
   siteMetadata: {
     title: "Joey's New Website",
     siteUrl: "https://jsephora.gatsbyjs.io/",
-    description:
-      "A website I am building in order to refresh my ReactJS skills and learn about the new features of Gatsby 5",
+    description: "A website I am building in order to refresh my ReactJS skills and learn about the new features of Gatsby 5",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
 };
