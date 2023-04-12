46e5cfc029c4be815a11ece61e7ed55722e5c364
jsephora <joey@sephoradesign.com>
2023-04-12 14:14:35 +0900

    something somethhing

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import { Link, useStaticQuery, graphql } from "gatsby";
 import {
   container,
   heading,
+  navBar,
   navLinks,
   navLinkItem,
   navLinkText,
@@ -21,32 +22,36 @@ const Layout = ({ pageTitle, children }) => {
   `);
 
   return (
-    <div className={container}>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/blog" className={navLinkText}>
-              Blog
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
+    <>
+      <div className={navBar}>
+        <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+          <nav>
+            <ul className={navLinks}>
+              <li className={navLinkItem}>
+                <Link to="/" className={navLinkText}>
+                  Home
+                </Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link to="/about" className={navLinkText}>
+                  About
+                </Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link to="/blog" className={navLinkText}>
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </nav>
+      </div>
+      <div className={container}>
+        <main>
+          <h1 className={heading}>{pageTitle}</h1>
+          {children}
+        </main>
+      </div>
+    </>
   );
 };
 
