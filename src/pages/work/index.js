7778c1bb90ab70c477956e3eb0eb7e63b5c77452
jsephora <joey@sephoradesign.com>
2023-05-05 20:01:32 +0900

    added query for work page images

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -1,44 +1,52 @@
 import * as React from 'react'
-import {Link, graphql} from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-const WorkPage = ({data}) => {
-    return (
-        <Layout pageTitle="Work">
-            {data
-                .allMdx
-                .nodes
-                .map((node) => (
-                    <article key={node.id}>
-                        <h2>
-                            <Link to={`/work/${node.frontmatter.slug}`}>
-                                {node.frontmatter.title}
-                            </Link>
-                        </h2>
-                        <p>Posted: {node.frontmatter.hero_image.id}</p>
-                        <p>{node.excerpt}</p>
-                    </article>
-                ))
-            }       
-        </Layout>
-    )
+const BlogPage = ({ data }) => {
+  // const image = getImage(node.frontmatter.hero_image.GatsbyImage)
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <GatsbyImage image={node.frontmatter.hero_image.childImageSharp.gatsbyImageData} alt={node.frontmatter.hero_image_alt} />
+          </article>
+        ))
+      }
+    </Layout>
+  )
 }
 
-export const query = graphql `
-query MyQuery {
-  allMdx {
-    nodes {
-      frontmatter {
-        hero_image {
+export const query = graphql`
+  query
+    {
+      allMdx(sort: {frontmatter: {date: DESC}}) {
+        nodes {
+          frontmatter {
+            date(formatString: "MMMM D, YYYY")
+            title
+            slug
+            hero_image {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+            hero_image_alt
+          }
           id
         }
       }
     }
-  }
-}
 `
 
-export const Head = () => <Seo title="Work"/>
+export const Head = () => <Seo title="My Blog Posts" />
 
-export default WorkPage
+export default BlogPage
