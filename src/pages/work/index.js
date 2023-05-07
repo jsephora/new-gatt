ae90f78e4248ee8ea08a1408859410a8e1b5102f
jsephora <joey@sephoradesign.com>
2023-05-08 07:49:23 +0900

    big changes

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -4,24 +4,29 @@ import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { Container, Row, Col } from 'react-bootstrap'
+import { previewTile, tileMiddle, tileImage, tileOverlay } from '../../components/layout.module.scss'
 
 const WorkPage = ({ data }) => {
   // const image = getImage(node.frontmatter.hero_image.GatsbyImage)
   return (
-    <Layout pageTitle='Work'>
+    <Layout pageTitle='Work' subTitle='A small selection of projects I have worked on recently' >
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
           <Col md={6}>
-            <h2>
-              <Link to={`/work/${node.frontmatter.slug}`}>
-                <GatsbyImage
-                  image={node.frontmatter.hero_image.childImageSharp.gatsbyImageData}
-                  alt={node.frontmatter.hero_image_alt}
+            <div className={previewTile}>
+              <div className={tileOverlay}>
+                <Link to={`/work/${node.frontmatter.slug}`}>
+                  <GatsbyImage
+                    image={node.frontmatter.hero_image.childImageSharp.gatsbyImageData}
+                    // alt={node.frontmatter.hero_image_alt}
+                    className={tileImage}
                   />
-                {node.frontmatter.title}
-              </Link>
-            </h2>
-            {/* <p>Posted: {node.frontmatter.date}</p> */}
+                  <div className={tileMiddle}>
+                    <h2>{node.frontmatter.title}</h2>
+                  </div>
+                </Link>
+              </div>
+            </div>
           </Col>
         </article>
       ))}
@@ -31,7 +36,7 @@ const WorkPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allMdx(sort: { frontmatter: { date: DESC } }) {
+    allMdx(sort: { frontmatter: { date: ASC } }) {
       nodes {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
