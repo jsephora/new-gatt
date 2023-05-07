8825b9e400686c61ddcd127ab7e7f5114999514c
jsephora <joey@sephoradesign.com>
2023-05-07 11:18:15 +0900

    not sure what the changes were

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -2,51 +2,54 @@ import * as React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import { Container, Row, Col } from 'react-bootstrap'
 
-const BlogPage = ({ data }) => {
+const WorkPage = ({ data }) => {
   // const image = getImage(node.frontmatter.hero_image.GatsbyImage)
   return (
-    <Layout pageTitle="My Blog Posts">
-      {
-        data.allMdx.nodes.map(node => (
-          <article key={node.id}>
+    <Layout pageTitle='Work'>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <Col md={6}>
             <h2>
-              <Link to={`/blog/${node.frontmatter.slug}`}>
+              <Link to={`/work/${node.frontmatter.slug}`}>
+                <GatsbyImage
+                  image={node.frontmatter.hero_image.childImageSharp.gatsbyImageData}
+                  alt={node.frontmatter.hero_image_alt}
+                  />
                 {node.frontmatter.title}
               </Link>
             </h2>
-            <p>Posted: {node.frontmatter.date}</p>
-            <GatsbyImage image={node.frontmatter.hero_image.childImageSharp.gatsbyImageData} alt={node.frontmatter.hero_image_alt} />
-          </article>
-        ))
-      }
+            {/* <p>Posted: {node.frontmatter.date}</p> */}
+          </Col>
+        </article>
+      ))}
     </Layout>
   )
 }
 
 export const query = graphql`
-  query
-    {
-      allMdx(sort: {frontmatter: {date: DESC}}) {
-        nodes {
-          frontmatter {
-            date(formatString: "MMMM D, YYYY")
-            title
-            slug
-            hero_image {
-              childImageSharp {
-                gatsbyImageData
-              }
+  query {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
+          hero_image {
+            childImageSharp {
+              gatsbyImageData
             }
-            hero_image_alt
           }
-          id
+          hero_image_alt
         }
+        id
       }
     }
+  }
 `
 
-export const Head = () => <Seo title="My Blog Posts" />
+export const Head = () => <Seo title='Work' />
 
-export default BlogPage
+export default WorkPage
